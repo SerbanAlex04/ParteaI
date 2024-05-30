27bcf10ea15c009592f888f6cd6953aaed994f40
Sherby22 <alex.serban22@icloud.com>
2024-05-30 03:43:25 +0300

    cerinta3 + histograme in output

---
+++ Surse/cerinta3.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('../titanic/train.csv')
+
+for col in df.axes[1]:
+    if df[col].dtypes == int or df[col].dtypes == float:
+        plt.hist(df[col])
+        plt.savefig(f'../output/cerinta3/hist_{col}')
