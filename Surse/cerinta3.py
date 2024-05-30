a5d4e253d673ba4887b98207b237206108398968
Sherby22 <alex.serban22@icloud.com>
2024-05-30 04:02:11 +0300

    cerinta3 + output corectat, se suprapuneau histogramele

--- Surse/cerinta3.py
+++ Surse/cerinta3.py
@@ -5,5 +5,9 @@ df = pd.read_csv('../titanic/train.csv')
 
 for col in df.axes[1]:
     if df[col].dtypes == int or df[col].dtypes == float:
+        plt.figure()
+        plt.title(col)
+        plt.xlabel('values')
+        plt.ylabel('frequencies')
         plt.hist(df[col])
         plt.savefig(f'../output/cerinta3/hist_{col}')
